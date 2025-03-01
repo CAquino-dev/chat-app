3e8c3759789f0f95c0484ead71ac6605e83eb31b
Christian.Dev <CAquino.dev@gmail.com>
2025-03-01 14:25:23 +0800

    create the routes for chat, login, and register pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Chat from './pages/Chat'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 const App = () => {
   return (
     <div>
-      Hello
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Login/>}/>
+          <Route path='/register' element={<Register/>}/>
+          <Route path='/chat' element={<Chat/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
